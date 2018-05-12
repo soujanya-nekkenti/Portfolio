461055665b19a505bdcd44c8a65aec4a233f2829
Soujanya Nekkanti <soujanya.nekkenti@gmail.com>
2018-05-12 21:46:23 +0200

    Second commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="demo-big-content">
+    <Layout>
+        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'black', fontFamily: 'Courgette', fontSize:'24px'}}
+                to="/">My Portfolio</Link>} scroll>
+            <Navigation>
+                <Link to="/resume" style={{ color: 'black', fontFamily: 'Satisfy', fontSize:'20px'}}>Resume</Link>
+                <Link to="/aboutMe" style={{ color: 'black', fontFamily: 'Satisfy', fontSize:'20px'}}>About Me</Link>
+                <Link to="/projects" style={{ color: 'black', fontFamily: 'Satisfy', fontSize:'20px'}}>Projects</Link>
+                <Link to="/contact" style={{ color: 'black', fontFamily: 'Satisfy', fontSize:'20px'}}>Contact</Link>
+            </Navigation>
+        </Header>
+        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}}
+                to="/">Home</Link>}>
+            <Navigation>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutMe">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+            <Main/>
+        </Content>
+    </Layout>
+</div>
+    );
+  }
+}
+
+export default App;
